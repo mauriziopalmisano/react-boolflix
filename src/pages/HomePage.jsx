271360c1f8d76dfeaa86f819499662cc90a61e36
mauriziopalmisano <maurizio.palmisano1@gmail.com>
2026-05-18 18:00:21 +0200

    aggiunta delle bandiere con la gestione di alcune bandiere che non si possono tradurre direttamente dalla lingua

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,18 +3,35 @@ import useMovies from "../hooks/useMovies";
 
 function HomePage() {
 
+  const lenguageExeption = {
+    en:'gb',
+    ja:'jp',
+    zh:'cn',
+    ko:'kr',
+    da:'dk',
+    el:'gr'
+  }
+
+const exeptionLenguageToCountry = (language) => {
+  return lenguageExeption[language] || language;
+};
+
   const { fetchedData } = useMovies();
 
   return (<>
     <div className="row">
       {fetchedData && fetchedData.results.map(movie => {
-        const { id, title, poster_path:img } = movie;
+        const { id, title, poster_path:img, original_language } = movie;
+        const imgURL = img ? `https://image.tmdb.org/t/p/w185${img}`: ' https://placehold.co/500x750?text=No+Poster';
+        const country = exeptionLenguageToCountry(original_language);
+        const flagURL = `https://flagcdn.com/16x12/${country}.png`;
         return (
           <div className="col-3" key={id}>
             <div className="card">
-              <img src={`https://image.tmdb.org/t/p/w185${img}`} className="card-img-top" alt={title} />
+              <img src={imgURL} className="card-img-top" alt={title} />
               <div className="card-body">
                 <h5 className="card-title">{title}</h5>
+                <img className="img-fluid" src={flagURL} alt="" />
               </div>
             </div>
           </div>
